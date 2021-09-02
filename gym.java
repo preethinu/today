e172c6f43607765eeea624cfad24ca080691b1eb
preethinu <preethi.xworkz@gmail.com>
2021-09-02 16:25:44 +0530

    added java file

---
+++ gym.java
@@ -0,0 +1,14 @@
+class Gym{
+static String gymName="fittness";
+static String trainer="arvind";
+static int cost=3000;
+
+public static void main(String a[]){
+
+System.out.println(gymName);
+System.out.println(trainer);
+System.out.println(cost);
+
+
+}
+}
