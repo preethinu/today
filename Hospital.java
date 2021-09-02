4415dc2848749b8e91ebf716d5d797acf52be31e
preethinu <preethi.xworkz@gmail.com>
2021-09-02 16:24:01 +0530

    added java file

---
+++ Hospital.java
@@ -0,0 +1,13 @@
+class Hospital{
+public static void main(String a[]){
+String hospitalname="KSR";
+String doctor ="Shivkumar";
+int opd = 20;
+System.out.println(hospitalname);
+System.out.println(doctor);
+System.out.println(opd);
+
+}
+}
+
+
